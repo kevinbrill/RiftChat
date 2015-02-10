cffe8814633dafe17c8e3579fad32c76ccd94416
Kevin Brill <kevin.brill@gmail.com>
2015-02-10 15:46:17 -0500

    Added indicator if the person coming online/offline is mobile.  Focus is shifted back to the text entry once a message has been sent

--- src/RiftChat/MainWindow.cs
+++ src/RiftChat/MainWindow.cs
@@ -47,13 +47,13 @@ public partial class MainWindow: Gtk.Window
 
 		chatClient.Login += (sender, e) => {
 			Application.Invoke( delegate {
-				WriteMessage( string.Format("{0} has come online.", e.Character.Name), LOGIN_LOGOUT );
+				WriteMessage( string.Format("{0}{1} has come online.", e.Character.Name, !e.InGame ? " (m)" : ""), LOGIN_LOGOUT );
 			});
 		};
 
 		chatClient.Logout += (sender, e) => {
 			Application.Invoke( delegate {
-				WriteMessage( string.Format("{0} has gone offline.", e.Character.Name), LOGIN_LOGOUT );
+				WriteMessage( string.Format("{0}{1} has gone offline.", e.Character.Name, !e.InGame ? " (m)" : ""), LOGIN_LOGOUT );
 			});
 		};
 
@@ -69,6 +69,10 @@ public partial class MainWindow: Gtk.Window
 			}
 
 			chatClient.SendGuildMessage( entryChat.Text.Trim() );
+			entryChat.Text = string.Empty;
+			if( entryChat.CanFocus ){
+				entryChat.GrabFocus();
+			}
 		};
 	}
 
@@ -88,7 +92,6 @@ public partial class MainWindow: Gtk.Window
 		var tag = new TextTag(LOGIN_LOGOUT);
 		tag.Weight = Pango.Weight.Light;
 		tag.ForegroundGdk = new Gdk.Color( 190, 190, 190 );
-
 		tags.Add (tag);
 
 		tag = new TextTag (GUILD_CHAT);
@@ -103,10 +106,11 @@ public partial class MainWindow: Gtk.Window
 	{
 		var iter = this.textviewChat.Buffer.EndIter;
 
-		var formattedMessage = string.Format ("{0} {1}{2}", dateTime == null ? DateTime.Now.ToString ("HH:mm:ss") : dateTime.Value.ToString ("HH:mm:ss"), WebUtility.UrlDecode (message), System.Environment.NewLine);
+		var formattedMessage = string.Format ("{0} {1}{2}", dateTime == null ? DateTime.Now.ToString ("HH:mm:ss") : dateTime.Value.ToString ("HH:mm:ss"), WebUtility.HtmlDecode (message), System.Environment.NewLine);
 
 		try {
 			this.textviewChat.Buffer.InsertWithTagsByName(ref iter, formattedMessage, tagName);	
+			this.textviewChat.ScrollToIter( this.textviewChat.Buffer.EndIter, 0, false, 0, 0 );
 		} catch (Exception ex) {
 			Debug.WriteLine (ex.ToString ());
 		}
