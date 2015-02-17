7ae0c65f4506d64bcdef0636d5141ab8041956d1
Kevin Brill <kevin.brill@gmail.com>
2015-02-17 10:05:47 -0500

    Added new widget for the chat window and moved most UI functionality out of main window

---
+++ src/RiftChat/ChatWidget.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using RiftChat.Common;
+using System.Diagnostics;
+using System.Net;
+using Gtk;
+using System.Collections.Generic;
+
+namespace RiftChat
+{
+	[System.ComponentModel.ToolboxItem (true)]
+	public partial class ChatWidget : Gtk.Bin, IChatView
+	{
+		private const string LOGIN_LOGOUT = "LoginLogout";
+		private const string GUILD_CHAT = "GuildChat";
+
+		public ChatWidget ()
+		{
+			this.Build ();
+
+			this.textviewChat.ModifyBase(StateType.Normal, new Gdk.Color(0,0,0));
+
+			foreach (var tag in GetNamedTags()) {
+				this.textviewChat.Buffer.TagTable.Add (tag);
+			}
+
+			buttonSend.Clicked += (sender, e) => {
+				if( string.IsNullOrWhiteSpace( entryChat.Text ) ) {
+					return;
+				}
+
+				if( this.SendMessage != null ) 
+					this.SendMessage( this, entryChat.Text.Trim() );
+
+				entryChat.Text = string.Empty;
+				if( entryChat.CanFocus ){
+					entryChat.GrabFocus();
+				}
+			};
+		}
+
+		public event EventHandler<string> SendMessage;
+
+		public void MessageReceived (rift.net.Message message)
+		{
+			Application.Invoke (delegate {
+				WriteMessage (string.Format ("{0}: {1}", message.Sender.Name, message.Text), GUILD_CHAT);
+			});
+		}
+
+		private List<TextTag> GetNamedTags()
+		{
+			var tags = new List<TextTag> ();
+
+			var tag = new TextTag(LOGIN_LOGOUT);
+			tag.Weight = Pango.Weight.Light;
+			tag.ForegroundGdk = new Gdk.Color( 190, 190, 190 );
+			tags.Add (tag);
+
+			tag = new TextTag (GUILD_CHAT);
+			tag.Weight = Pango.Weight.Normal;
+			tag.ForegroundGdk = new Gdk.Color (50, 205, 50);
+			tags.Add (tag);
+
+			return tags;
+		}
+
+		private void WriteMessage(string message, string tagName, DateTime? dateTime = null)
+		{
+			var iter = this.textviewChat.Buffer.EndIter;
+
+			var formattedMessage = string.Format ("{0} {1}{2}", dateTime == null ? DateTime.Now.ToString ("HH:mm:ss") : dateTime.Value.ToString ("HH:mm:ss"), WebUtility.HtmlDecode (message), System.Environment.NewLine);
+
+			try {
+				this.textviewChat.Buffer.InsertWithTagsByName(ref iter, formattedMessage, tagName);	
+				this.textviewChat.ScrollToIter( this.textviewChat.Buffer.EndIter, 0, false, 0, 0 );
+			} catch (Exception ex) {
+				Debug.WriteLine (ex.ToString ());
+			}
+		}
+	}
+}
+
--- src/RiftChat/MainWindow.cs
+++ src/RiftChat/MainWindow.cs
@@ -10,26 +10,18 @@ using RiftChat.Common;
 
 public partial class MainWindow: Gtk.Window
 {
-	private const string LOGIN_LOGOUT = "LoginLogout";
-	private const string GUILD_CHAT = "GuildChat";
-
 	private RiftChatClient chatClient;
 	ContactController friendsController;
 	ContactController guildiesController;
+	ChatController guildChatController;
 
 	public MainWindow () : base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
 
-		this.textviewChat.ModifyBase(StateType.Normal, new Gdk.Color(0,0,0));
-
-		foreach (var tag in GetNamedTags()) {
-			this.textviewChat.Buffer.TagTable.Add (tag);
-		}
-
 		var sessionFactory = new SessionFactory ();
 
-		var session = sessionFactory.Login ("","");
+		var session = sessionFactory.Login ("", "");
 
 		var securedClient = new RiftClientSecured (session);
 
@@ -43,47 +35,11 @@ public partial class MainWindow: Gtk.Window
 		friendsController = new ContactController (chatClient) { View = (IContactView)treeview2, Model = friends };
 		guildiesController = new ContactController (chatClient) { View = (IContactView)treeview3, Model = guildies };
 
+		guildChatController = new ChatController (chatClient, ChatChannel.Guild) { View = chatwidgetGuild };
+
 		chatClient.Connect ();
 
 		chatClient.Listen ();
-
-		chatClient.Connected += (sender, e) => {
-			Application.Invoke( delegate {
-				var iter = this.textviewChat.Buffer.GetIterAtLine(this.textviewChat.Buffer.LineCount);
-
-				this.textviewChat.Buffer.Insert(ref iter, "Connected!\n");
-			});
-		};
-
-		chatClient.Login += (sender, e) => {
-			Application.Invoke( delegate {
-				WriteMessage( string.Format("{0}{1} has come online.", e.Character.Name, e.Location == Location.Web ? " (m)" : ""), LOGIN_LOGOUT );
-			});
-		};
-
-		chatClient.Logout += (sender, e) => {
-			Application.Invoke( delegate {
-				WriteMessage( string.Format("{0}{1} has gone offline.", e.Character.Name, e.Location == Location.Web ? " (m)" : ""), LOGIN_LOGOUT );
-			});
-		};
-
-		chatClient.GuildChatReceived += (sender, e) => {
-			Application.Invoke( delegate {
-				WriteMessage (string.Format ("{0}: {1}", e.Sender.Name, e.Text), GUILD_CHAT);
-			});
-		};
-
-		buttonSend.Clicked += (sender, e) => {
-			if( string.IsNullOrWhiteSpace( entryChat.Text ) ) {
-				return;
-			}
-
-			chatClient.SendGuildMessage( entryChat.Text.Trim() );
-			entryChat.Text = string.Empty;
-			if( entryChat.CanFocus ){
-				entryChat.GrabFocus();
-			}
-		};
 	}
 
 
@@ -93,36 +49,5 @@ public partial class MainWindow: Gtk.Window
 
 		Application.Quit ();
 		a.RetVal = true;
-	}
-
-	private List<TextTag> GetNamedTags()
-	{
-		var tags = new List<TextTag> ();
-
-		var tag = new TextTag(LOGIN_LOGOUT);
-		tag.Weight = Pango.Weight.Light;
-		tag.ForegroundGdk = new Gdk.Color( 190, 190, 190 );
-		tags.Add (tag);
-
-		tag = new TextTag (GUILD_CHAT);
-		tag.Weight = Pango.Weight.Normal;
-		tag.ForegroundGdk = new Gdk.Color (50, 205, 50);
-		tags.Add (tag);
-
-		return tags;
-	}
-
-	private void WriteMessage(string message, string tagName, DateTime? dateTime = null)
-	{
-		var iter = this.textviewChat.Buffer.EndIter;
-
-		var formattedMessage = string.Format ("{0} {1}{2}", dateTime == null ? DateTime.Now.ToString ("HH:mm:ss") : dateTime.Value.ToString ("HH:mm:ss"), WebUtility.HtmlDecode (message), System.Environment.NewLine);
-
-		try {
-			this.textviewChat.Buffer.InsertWithTagsByName(ref iter, formattedMessage, tagName);	
-			this.textviewChat.ScrollToIter( this.textviewChat.Buffer.EndIter, 0, false, 0, 0 );
-		} catch (Exception ex) {
-			Debug.WriteLine (ex.ToString ());
-		}
 	}
 }
